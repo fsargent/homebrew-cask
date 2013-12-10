4f2bfa82e69a610d5a592bfbb109366b30d19ba3
Felix Sargent <felix.sargent@gmail.com>
2013-12-09 19:23:49 -0800

    Upgrade Vagrant to 1.4.0
    http://www.vagrantup.com/downloads.html
    http://www.vagrantup.com/blog/vagrant-1-4.html

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,8 +1,8 @@
 class Vagrant < Cask
-  url 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg'
+  url 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.0.dmg'
   homepage 'http://www.vagrantup.com'
-  version '1.3.5'
-  sha1 'c123b4f80abd9cec83f7499d8dc2bc60b850b882'
+  version '1.4.'
+  sha1 '58731df082bb0fbfa36c8e567419a9921232e6d875466f338b4416493859a59e'
   install 'Vagrant.pkg'
   uninstall :script => 'uninstall.tool', :input => %w[Yes]
 end
